ede6a836213787d467179bd7662f01a8b8b02569
Mohammad Iqbal <mohiqbal954@gmail.com>
2021-07-14 22:38:57 -0400

    Removed Table Title Component

--- client/src/components/table-title/TableTitle.jsx
+++
@@ -1,8 +0,0 @@
-import './tableTitle.scss'
-export default function TableTitle(props) {
-    let titleMsg = props.homePage ? "Going Bad This Week!" : "List of Everything"
-
-    return (
-        <div className="table-title">{titleMsg}</div>
-    )
-}
